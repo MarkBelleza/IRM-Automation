bda402e08845742d5166a3271781928507b32441
Belleza <mark.belleza@ontario.ca>
2025-01-15 16:09:26 -0500

    test scripts for testcase IDs TC0034 TC0035

---
+++ src/test/java/ca/IRM/selenium/UserTypeAccess/StaffSergeant5.java
@@ -0,0 +1,159 @@
+package ca.IRM.selenium.UserTypeAccess;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import ca.IRM.selenium.pages.DetailsAndCircumstances;
+import ca.IRM.selenium.pages.IncidentTypeSelection;
+import ca.IRM.selenium.pages.Involved;
+import ca.IRM.selenium.pages.IsMediaAware;
+import ca.IRM.selenium.pages.Notification;
+import ca.IRM.selenium.pages.PoliceContacted;
+import ca.IRM.selenium.pages.RegionalOfficeDetails;
+import ca.IRM.selenium.pages.ReportPreparation;
+import ca.IRM.selenium.pages.ReportSearch;
+import ca.IRM.selenium.pages.StandardItemChecklist;
+import ca.IRM.selenium.pages.SupportingDocuments;
+import ca.IRM.selenium.pages.User;
+import ca.IRM.selenium.utils.DateTimeUI;
+import ca.IRM.selenium.utils.NavBar;
+import ca.IRM.selenium.utils.ReadXLSData;
+import ca.IRM.selenium.utils.WebUtils;
+
+public class StaffSergeant5 {
+	private NavBar nav;
+	private Notification notificationFields;
+	private DateTimeUI date;
+	private RegionalOfficeDetails regionalFields;
+	private IsMediaAware mediaFields;
+	private IncidentTypeSelection incidentFields;
+	private StandardItemChecklist checklist;
+	private SupportingDocuments support ;
+	private DetailsAndCircumstances details ;
+	private PoliceContacted contacted;
+	private Involved involve;
+	private ReportPreparation report;	
+	private User user;
+	private ReportSearch search;
+	private ReadXLSData loc;
+	private String[] secondary;
+	private WebDriverWait wait;
+//	FluentWait<EdgeDriver> wait;
+	
+	private EdgeDriver driver;
+	private WebUtils utils;
+	
+	
+	@BeforeTest(groups="testing")
+	public void setUpApplication() throws EncryptedDocumentException, IOException {
+//		Setup IRM application
+//		WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
+		utils = new WebUtils(driver);
+		nav = new NavBar(driver);
+		notificationFields = new Notification(driver);
+		date = new DateTimeUI(driver);
+		regionalFields = new RegionalOfficeDetails(driver);
+		mediaFields = new IsMediaAware(driver);
+		incidentFields = new IncidentTypeSelection(driver);
+		checklist = new StandardItemChecklist(driver);
+		support = new SupportingDocuments(driver);
+		details = new DetailsAndCircumstances(driver);
+		contacted = new PoliceContacted(driver);
+		involve = new Involved(driver);
+		report = new ReportPreparation(driver);		
+		user = new User(driver);
+		search = new ReportSearch(driver);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		loc = new ReadXLSData();
+		
+		WebUtils.setUpIrmPage(driver);
+	}
+	
+	
+	@AfterTest(groups="testing")
+	public void close() {
+//		driver.quit();
+		user.changeUserType(user.staff, user.algo, new String[0]); // work around to remove secondary locations
+		System.out.println("After Test");
+	}
+	
+//	TestCase ID: TC0034
+	@Test(dataProvider="xmls", dataProviderClass=ReadXLSData.class, groups="testing")
+	public void Locations(String location) throws EncryptedDocumentException, IOException {
+//		Refresh to make sure test retries properly
+		driver.navigate().refresh();
+	
+//		Bug: sometimes previous secondary location would still be saved when selecting a new secondary location
+//		Make sure to be able to use Algoma as a secondary location
+		String[] sec = {location};
+		if(location.equals(user.algo)) {
+			user.changeUserType(user.staff, user.brock, sec);
+		}else {
+			user.changeUserType(user.staff, user.algo, sec);
+		}	
+		
+		nav.createNewReport();
+		
+//		Fill in the appropriate fields in Notification (set location to within the user location, ALGOMA)
+		notificationFields.selectPriority("One");
+		notificationFields.selectLocation(location + " (Institution)");
+		notificationFields.selectArea("Washroom");
+		notificationFields.clickNext();
+		utils.duplicatePopUpCheck();
+		
+//		** Store the Incident Report ID
+		regionalFields.verifyPage();
+		String IncidentID = regionalFields.getIncidentID();
+		System.out.println("Created Incident ID: " + IncidentID);
+		
+		regionalFields.clickNext();
+		
+		mediaFields.clickNext();
+		
+//		Both IIR and EOIR incident type should be visible
+		incidentFields.verifyPage();
+		Assert.assertEquals(incidentFields.verifyIIR(), incidentFields.verifyEOIR());
+		
+//		Select IIR and EOIR incident types
+		incidentFields.expandItem("IIR");
+		incidentFields.expandItem("Assault");
+		incidentFields.expandItem("(P1) Serious Inmate on Inmate");
+		incidentFields.expandItem("Item thrown/contact");
+		incidentFields.selectItem("Bodily substance");
+		
+		incidentFields.expandItem("EOIR");
+		incidentFields.expandItem("Death of Staff");
+		incidentFields.selectItem("Off Duty");
+		
+		incidentFields.clickNext();
+		
+		checklist.clickNext();
+		
+		support.clickNext();
+		
+		details.clickNext();
+		
+		contacted.selectReason(contacted.notPoliceMatter);
+		contacted.clickNext();
+		
+		involve.clickNext();
+		
+		report.selectContactPerson("Mark", "Belleza");
+		report.finalize(); 
+		report.clickSubmit();
+		
+//		Verify Incident Report is saved
+		search.searchIncidentReport(IncidentID);
+		search.verifyIncident(IncidentID, "IIR, Assault, EOIR, Death of Staff"); //level 2 and 3 are not displayed
+	}
+	
+}
