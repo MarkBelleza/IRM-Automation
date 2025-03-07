d7c47cb19ef4665e858f771ef563e4246da42f2a
Belleza <mark.belleza@ontario.ca>
2025-03-07 17:14:57 -0500

    added feature to fill in the other text fields in PoliceContacted page

--- src/main/java/ca/IRM/selenium/pages/PoliceContacted.java
+++ src/main/java/ca/IRM/selenium/pages/PoliceContacted.java
@@ -3,10 +3,16 @@ package ca.IRM.selenium.pages;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import ca.IRM.selenium.components.SearchTables;
 
 public class PoliceContacted {
 
@@ -20,6 +26,15 @@ public class PoliceContacted {
 	
 	By cancelButton = By.xpath("//span[@class='mud-button-label' and text()='Cancel']");
 	By updateButton = By.xpath("//span[@class='mud-button-label' and text()='Update']");
+	By searchNameButton = By.xpath("//span[@class='mud-button-label' and text()='Search']");
+	
+	By policeContactedField = By.xpath("/html/body/div[3]/div/div/div/div[2]/form/div[1]/div[7]/div/div/div/input");
+	By policeContactedByField = By.xpath("/html/body/div[3]/div/div/div/div[2]/form/div[1]/div[8]/div/div/div/input");
+	By policeServiceField = By.xpath("/html/body/div[3]/div/div/div/div[2]/form/div[1]/div[10]/div/div/div/input");
+	By policeContactedMethod = By.xpath("/html/body/div[3]/div/div/div/div[2]/form/div[1]/div[11]/div/div/div/input");
+	By policeTelephoneField = By.xpath("//input[@class='mud-input-slot mud-input-root mud-input-root-outlined' and @type='tel']");
+	By policeCaseField = By.xpath("/html/body/div[3]/div/div/div/div[2]/form/div[1]/div[13]/div/div/div/input");
+	
 	
 	String dropDownFields = "mud-input-slot mud-input-root mud-input-root-outlined mud-input-root-adorned-end mud-select-input";
 	
@@ -64,8 +79,12 @@ public class PoliceContacted {
 		String radio = "/following-sibling::div[@class='" + radioContainer + "']//span[@class='" + radioLabel 
 					+ "' and text()='" + yesNo + "']/preceding-sibling::span[@class='" + radioButton + "']";
 
-		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(By.xpath(label + radio)))).perform();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(label + radio))).click();
+		try {
+			actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(By.xpath(label + radio)))).perform();
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath(label + radio))).click();
+		}catch(TimeoutException e) {
+			throw new NoSuchElementException("'" + yesNo + "' may have already been selected in 'Will Police be Attending' field from the Police Contacted page. Please double check the radio status or selector used!");
+		}
 		
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(label + radio)));
 	}
@@ -75,11 +94,114 @@ public class PoliceContacted {
 		String label = "//p[@class='" + radioTitle + "' and text()='Criminal Charges']";
 		String radio = "/following-sibling::div[@class='" + radioContainer + "']//span[@class='" + radioLabel 
 					+ "' and text()='" + info + "']/preceding-sibling::span[@class='" + radioButton + "']";
-
-		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(By.xpath(label + radio)))).perform();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(label + radio))).click();
+		try {
+			actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(By.xpath(label + radio)))).perform();
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath(label + radio))).click();
+		}catch(TimeoutException e) {
+			throw new NoSuchElementException("'" + info + "' may have already been selected in 'Criminal Charges' field from the Police Contacted page. Please double check the radio status or selector used!");
+		}
 		
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(label + radio)));
+	}
+	
+	public void fillPersonContacted(String details) {
+		Actions actions = new Actions(driver);
+		
+		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(policeContactedField))).click().perform();
+        actions.keyDown(wait.until(ExpectedConditions.elementToBeClickable(policeContactedField)), Keys.CONTROL)  // Press CTRL
+        .sendKeys("A")                   // Press A (to select all)
+        .sendKeys(Keys.BACK_SPACE)           // Press BACKSPACE (to delete the text)
+        .keyUp(Keys.CONTROL)                // Release CTRL
+        .perform();                         // .clear() was not working
+		wait.until(ExpectedConditions.elementToBeClickable(policeContactedField)).sendKeys(details);
+		
+	}
+	
+	public void fillPersonContactedBy(String name) {
+		Actions actions = new Actions(driver);
+		SearchTables table = new SearchTables(driver);
+		
+		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(policeContactedByField))).click().perform();
+        actions.keyDown(wait.until(ExpectedConditions.elementToBeClickable(policeContactedByField)), Keys.CONTROL)  // Press CTRL
+        .sendKeys("A")                   // Press A (to select all)
+        .sendKeys(Keys.BACK_SPACE)           // Press BACKSPACE (to delete the text)
+        .keyUp(Keys.CONTROL)                // Release CTRL
+        .perform();                         // .clear() was not working
+		wait.until(ExpectedConditions.elementToBeClickable(policeContactedByField)).sendKeys(name);
+		
+		wait.until(ExpectedConditions.elementToBeClickable(searchNameButton)).click();
+		
+		
+		String[] parts = name.split(" ");
+		String first = parts[0];
+		String last;
+		
+		if (parts.length == 1) {
+			last = "";
+		}else {
+			last = parts[1];
+		}
+		
+		table.selectUserFromTable(first, last);
+	}
+	
+	public void fillPoliceService(String details) {
+		Actions actions = new Actions(driver);
+		
+		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(policeServiceField))).click().perform();
+        actions.keyDown(wait.until(ExpectedConditions.elementToBeClickable(policeServiceField)), Keys.CONTROL)  // Press CTRL
+        .sendKeys("A")                   // Press A (to select all)
+        .sendKeys(Keys.BACK_SPACE)           // Press BACKSPACE (to delete the text)
+        .keyUp(Keys.CONTROL)                // Release CTRL
+        .perform();                         // .clear() was not working
+		wait.until(ExpectedConditions.elementToBeClickable(policeServiceField)).sendKeys(details);
+		
+	}
+	
+	
+	public void fillPoliceContactedMethod(String details) {
+		Actions actions = new Actions(driver);
+		
+		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(policeContactedMethod))).click().perform();
+        actions.keyDown(wait.until(ExpectedConditions.elementToBeClickable(policeContactedMethod)), Keys.CONTROL)  // Press CTRL
+        .sendKeys("A")                   // Press A (to select all)
+        .sendKeys(Keys.BACK_SPACE)           // Press BACKSPACE (to delete the text)
+        .keyUp(Keys.CONTROL)                // Release CTRL
+        .perform();                         // .clear() was not working
+		wait.until(ExpectedConditions.elementToBeClickable(policeContactedMethod)).sendKeys(details);
+		
+	}
+	
+	/**
+	 * Fill Police Telephone Field
+	 * 
+	 * @param details (String: ie. "6471231234". Only string numerals)
+	 */
+	public void fillPoliceTelephone(String details) {
+		Actions actions = new Actions(driver);
+		
+		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(policeTelephoneField))).click().perform();
+        actions.keyDown(wait.until(ExpectedConditions.elementToBeClickable(policeTelephoneField)), Keys.CONTROL)  // Press CTRL
+        .sendKeys("A")                   // Press A (to select all)
+        .sendKeys(Keys.BACK_SPACE)           // Press BACKSPACE (to delete the text)
+        .keyUp(Keys.CONTROL)                // Release CTRL
+        .perform();                         // .clear() was not working
+        wait.until(ExpectedConditions.elementToBeClickable(policeTelephoneField)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(policeTelephoneField)).sendKeys(details);
+		
+	}
+	
+	public void fillPoliceCase(String details) {
+		Actions actions = new Actions(driver);
+		
+		actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(policeCaseField))).click().perform();
+        actions.keyDown(wait.until(ExpectedConditions.elementToBeClickable(policeCaseField)), Keys.CONTROL)  // Press CTRL
+        .sendKeys("A")                   // Press A (to select all)
+        .sendKeys(Keys.BACK_SPACE)           // Press BACKSPACE (to delete the text)
+        .keyUp(Keys.CONTROL)                // Release CTRL
+        .perform();                         // .clear() was not working
+		wait.until(ExpectedConditions.elementToBeClickable(policeCaseField)).sendKeys(details);
+		
 	}
 	
 	public void clickNext() {
